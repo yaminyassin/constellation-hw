712e95025d26d4d5729b5412b3176b086a671316
Yamin Yassin <yamin2008@live.com>
2025-07-13 20:29:00 +0100

    feat(mocks): added mocks and models of cars

---
+++ models/Vehicles.ts
@@ -0,0 +1,11 @@
+export interface Vehicle {
+  make: string;
+  model: string;
+  engineSize: string;
+  fuel: string;
+  year: number;
+  mileage: number;
+  auctionDateTime: string;
+  startingBid: number;
+  favourite: boolean;
+}
